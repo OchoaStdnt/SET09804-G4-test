6161fbf9ab63cb357061025f5ba4dd9cbfa04900
OchoaStdnt <40688673@live.napier.ac.uk>
2024-11-27 00:38:48 -0600

    finished all current methods

--- src/main/java/com/napier/devops/Methods.java
+++ src/main/java/com/napier/devops/Methods.java
@@ -5,6 +5,7 @@ public class Methods {
 
     private Connection con = null;
 
+    /* add colors to the system out */
     final String RESET = "\u001B[0m";
     final String BLUE = "\u001B[34m";
     final String GREEN = "\u001B[32m";
