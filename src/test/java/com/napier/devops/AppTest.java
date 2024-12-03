d78c48726903e0797d18a4a336ba3acc57274539
Ochoa-88 <157509216+Ochoa-88@users.noreply.github.com>
2024-12-02 21:49:56 -0600

    finished all current methods

---
+++ src/test/java/com/napier/devops/AppTest.java
@@ -0,0 +1,26 @@
+package com.napier.devops;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest
+{
+    static App app;
+
+    @BeforeAll
+    static void init()
+    {
+        app = new App();
+    }
+
+    @Test
+    void showWorldDBTablesNull()
+    {
+        app.showWorldDBTables(null);
+    }
+}
